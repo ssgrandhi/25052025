b9c5b95c1a0f3b370f90fb92e45d1560c3716389
developer <developer@gmail.com>
2025-06-01 19:45:35 +0530

    Added console

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("add theline")
 root.render(
   <React.StrictMode>
     <App />
